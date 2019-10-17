160ea2cc1a8f6f3b257290ea2f4a8fc290d8b73c
Barak Saidoff <barak.saidoff@gmail.com>
2019-10-17 18:04:41 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,23 +11,39 @@ const codes = [
   "a"
 ];
 
-function init() {
-  var index=0;
-  document.body.addEventListener('keydown', onKeyDownHandler);
-  
-  function oneKeyDownHandler(e){
-    const key = parseInt(e.details || e.which)
-    console.log('key', key)
-    if (key === codes[index]) {
-      index++
-      
-      if (index===codes.length){
-        alert "Nice Job!"
-        index=0
-      } else {
-        index =0
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
+
+function init(){
+
+
+var body = document.body;
+  body.addEventListener('keydown', function(e){
+    var key = parseInt(e.detail || e.which);
+
+    // check if key is equal to current value of index in code array
+    // then loop through the array if conditions match
+
+    if (key === code[index]) {
+      // if yes, increment index by 1
+      index++;
+
+      // if still in the loop, check if index is equal to code array length
+      if (index === code.length - 1) {
+        // reset the index counter
+        index = 0;
+        // if yes, you passed, congrats!
+        alert("Congrats!");
       }
-    }
+
+
+
+  // if key doesn't match current index in code array, then reset the index to zero
+  } else {
+    index = 0;
   }
+
+
+  });
+
 }
-init()
